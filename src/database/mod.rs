1a8dded364659b1dcacd4d00833cb7cc1288e2b7
LuckShiba <LuckShiba@protonmail.com>
2021-04-07 22:44:28 -0300

    removing database

--- src/database/mod.rs
+++
@@ -1,6 +0,0 @@
-use mongodb::{Client, Database, error::Error};
-pub mod guild;
-
-pub async fn create_database<'a>(uri: &'a str) -> Result<Database, Error>{
-    Ok(Client::with_uri_str(uri).await?.database("shiber"))
-}
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,31 @@
 mod commands;
 mod events;
-pub mod database;
 pub mod constants;
 
-use database::create_database;
 use dotenv::dotenv;
 use events::Events;
-use mongodb::Database;
 
 use std::env;
 
-use serenity::{client::Client, prelude::TypeMapKey};
+use serenity::client::Client;
 
 use commands::configure_commands;
 
 extern crate dotenv;
 
-struct BotDatabase;
-
-impl TypeMapKey for BotDatabase {
-    type Value = Database;
-}
 
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
-    match create_database(&env::var("MONGO_URI").expect("mongo_uri")[..]).await {
-        Ok(res) => {
-            let mut client = Client::builder(
-                env::var("TOKEN")
-                    .expect("Invalid token."))
-                .event_handler(Events)
-                .framework(configure_commands())
-                .await
-                .expect("Error creating client.");
-            {
-                let mut data = client.data.write().await;
-                data.insert::<BotDatabase>(res);
-            }
+    dotenv().ok();            
+    let mut client = Client::builder(
+        env::var("TOKEN")
+            .expect("Invalid token."))
+        .event_handler(Events)
+        .framework(configure_commands())
+        .await
+        .expect("Error creating client.");
 
-            if let Err(why) = client.start().await {
-                println!("Error durring starting client: {:?}", why);
-            }
-        }
-        Err(why) => 
-            println!("Error connecting to database: {:?}", why)   
-    }
+    if let Err(why) = client.start().await {
+        println!("Error durring starting client: {:?}", why);
+}
 }
